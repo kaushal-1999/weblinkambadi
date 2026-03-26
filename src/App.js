2f7d0d10eb7e0d8e7dc08ccdac017d61cb62cb82
Kaushal Patil <kaushalpatil.tech@gmail.com>
2026-03-27 01:20:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer/Footer";
+// import Home from "./pages/Home";
+// // import Home from "./pages/Home";
+// // import Services from "./pages/Services";
+// // import About from "./pages/About";
+// // import Areas from "./pages/Areas";
+// // import Contact from "./pages/Contact";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+
+//       {/* <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/services" element={<Services />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/areas" element={<Areas />} />
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes> */}
+//       <Home/>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
-// import Home from "./pages/Home";
-// import Services from "./pages/Services";
-// import About from "./pages/About";
-// import Areas from "./pages/Areas";
-// import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <Navbar />
-
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/areas" element={<Areas />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes> */}
-      <Home/>
+      <Home />
       <Footer />
     </Router>
   );
